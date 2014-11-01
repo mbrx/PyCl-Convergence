b54b9d3ab5804a9a8064b951bff68b2e3c05ed4b
Mathias Broxvall <mathias.broxvall@gmail.com>
2014-11-01 21:42:32 +0100

    option for multiple executions between  each readback

--- ConwayCL-Final/main.py
+++ ConwayCL-Final/main.py
@@ -2,6 +2,7 @@ import pyopencl as cl
 import numpy as np
 import random as r
 import datetime as date
+import time 
 
 np.set_printoptions(threshold=np.nan)
 
@@ -13,71 +14,90 @@ np.set_printoptions(threshold=np.nan)
 
 
 class CL:
-	def __init__(self):
-		self.ctx = cl.create_some_context()
-		self.queue = cl.CommandQueue(self.ctx)
+        def __init__(self):
+                self.ctx = cl.create_some_context()
+                self.queue = cl.CommandQueue(self.ctx)
+
+                self.executionsPerReadback=1
 
 	#Load kernal file and load as internal program
-	def loadProgram(self, filename):
-		#read in the OpenCL source file as a string
-		f = open(filename, 'r')
-		fstr = "".join(f.readlines())
-		#print fstr
-		#create the program
-		self.program = cl.Program(self.ctx, fstr).build()
+        def loadProgram(self, filename):
+                #read in the OpenCL source file as a string
+                f = open(filename, 'r')
+                fstr = "".join(f.readlines())
+                #print fstr
+                #create the program
+                self.program = cl.Program(self.ctx, fstr).build()
 
 	#Create the host structures and buffers
-	def popCorn(self):
-		mf = cl.mem_flags
-		
+        def popCorn(self):
+                mf = cl.mem_flags
+                
 		#initialize client side (CPU) arrays
-		#Use ar_ySize to increase the worldspace
-		self.ar_ySize = np.int32(36)
-		self.a = np.ones((self.ar_ySize,self.ar_ySize), dtype=np.int32)
-		self.c = np.ones((self.ar_ySize,self.ar_ySize), dtype=np.int32)
-		#create OpenCL buffers
-		self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.a)
-		self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.a.nbytes)
+                #Use ar_ySize to increase the worldspace
+                self.ar_ySize = np.int32(1024)
+                self.a = np.ones((self.ar_ySize,self.ar_ySize), dtype=np.int32)
+                self.c = np.ones((self.ar_ySize,self.ar_ySize), dtype=np.int32)
+                #create OpenCL buffers
+                self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.a)
+                self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.a.nbytes)
 
 	#Run Kernal, create buffer, fill buffer
-	def execute(self):
-		self.program.Conway(self.queue, self.a.shape, None, self.ar_ySize, self.a_buf, self.dest_buf)
-		cl.enqueue_read_buffer(self.queue, self.dest_buf, self.c).wait()
-		self.a = self.c;
-		#Refresh buffers
-		mf = cl.mem_flags
-		self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.a)
-		self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.a.nbytes)
+        def execute(self):
+                if self.executionsPerReadback == 1:
+                        self.program.Conway(self.queue, self.a.shape, None, self.ar_ySize, self.a_buf, self.dest_buf)
+                else:
+                        for i in range(0,self.executionsPerReadback/2):
+                                self.program.Conway(self.queue, self.a.shape, None, self.ar_ySize, self.a_buf, self.dest_buf)
+                                self.program.Conway(self.queue, self.a.shape, None, self.ar_ySize, self.dest_buf, self.a_buf)
+                                cl.enqueue_barrier(self.queue)
+                        if self.executionsPerReadback%2 == 1:
+                                self.program.Conway(self.queue, self.a.shape, None, self.ar_ySize, self.a_buf, self.dest_buf)
+
+                if self.executionsPerReadback%2 == 1:
+                        cl.enqueue_read_buffer(self.queue, self.dest_buf, self.c).wait()
+                else: 
+                        cl.enqueue_read_buffer(self.queue, self.a_buf, self.c).wait()
+
+                self.a = self.c;
+                #Refresh buffers
+                mf = cl.mem_flags
+                self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.a)
+                self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.a.nbytes)
 
 	#Run Kernal, create buffer, fill buffer
-	def seed(self):
-		self.c = np.int32(np.random.randint(2, size=(self.ar_ySize, self.ar_ySize)))
-		self.a = self.c
-		#Refresh buffers
-		mf = cl.mem_flags
-		self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.a)
-		self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.a.nbytes)
+        def seed(self):
+                self.c = np.int32(np.random.randint(2, size=(self.ar_ySize, self.ar_ySize)))
+                self.a = self.c
+                #Refresh buffers
+                mf = cl.mem_flags
+                self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.a)
+                self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.a.nbytes)
 
 	#Print the output array
-	def render(self):
-		print self.a
-	
+        def render(self):
+                print self.a
+                
 if __name__ == "__main__":
-	example = CL()
-	example.loadProgram("kTest.cl")
-	example.popCorn()
+        example = CL()
+        example.executionsPerReadback=10
+        example.loadProgram("kTest.cl")
+        example.popCorn()
 
 	print "Begin CPU Seed:", date.datetime.now()
-	example.seed()	
+        example.seed()	
 
 	print "Begin GPU Loop:", date.datetime.now()
-	for i in range(5000):
-		example.execute()
+        time1=time.clock()
+        for i in range(50000/example.executionsPerReadback):
+                example.execute()
+        time2=time.clock()
 
-	print "Begin CPU Render:", date.datetime.now()
-	example.render()
+	#print "Begin CPU Render:", date.datetime.now()
+        #example.render()
 
 	print "Done", date.datetime.now()
+        print "GPU time: "+unicode(time2-time1)
 
 
 
